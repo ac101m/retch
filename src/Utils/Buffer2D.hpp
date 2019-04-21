40631d98f49bc58c0f2c57266fc0d88b6834f47f
ac101m <mofty101@gmail.com>
2019-04-21 23:31:53 +0100

    Add width and height member variables

--- src/Utils/Buffer2D.hpp
+++ src/Utils/Buffer2D.hpp
@@ -1,7 +1,7 @@
 #ifndef _RETCH_BUFFER_INCLUDED
 #define _RETCH_BUFFER_INCLUDED
 
-
+// Standard
 #include <vector>
 
 
@@ -10,24 +10,37 @@ namespace retch {
   template<class T>
   class Buffer2D {
     private:
+      unsigned width;
+      unsigned height;
+
       // Buffer data storage
-      std::vector<T> data;
+      std::vector<T> dataVector;
       std::vector<T*> index;
 
     public:
       Buffer2D(int const width, int const height) {
-        this->data = std::vector<T>(width * height);
+        this->width = width;
+        this->height = height;
+
+        this->dataVector = std::vector<T>(width * height);
         this->index = std::vector<T*>(height);
 
         for(unsigned i = 0; i < this->index.size(); i++) {
-          this->index[i] = &this->data[i * width];
+          this->index[i] = &this->dataVector[i * width];
         }
       }
 
-      // Overload access operator
+      // Overloaded access operator
       T*& operator[](int const i) {
         return this->index[i];
       }
+
+      // Get raw element pointer
+      std::vector<T>& data(void) {return this->dataVector;}
+
+      // Width and height gets
+      unsigned Width(void) {return this->width;}
+      unsigned Height(void) {return this->height;}
   };
 
 } // namespace retch
