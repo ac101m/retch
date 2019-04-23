36d18a222de454b08d0e32ad9d06642f2ee35e52
ac101m <mofty101@gmail.com>
2019-04-23 18:34:05 +0100

    Fix UV declared as vec3

--- src/Geometry/Vertex.hpp
+++ src/Geometry/Vertex.hpp
@@ -11,7 +11,7 @@ namespace retch {
   class Vertex {
   public:
     glm::vec3 position;
-    glm::vec3 uv;
+    glm::vec2 uv;
     glm::vec3 normal;
     glm::vec3 tangent;
     glm::vec3 bitangent;
