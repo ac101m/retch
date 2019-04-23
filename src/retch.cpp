a163db7a00e096e59c7b6fac230c1e2ec09e456c
ac101m <mofty101@gmail.com>
2019-04-23 18:46:48 +0100

    Update main with test pattern

--- src/retch.cpp
+++ src/retch.cpp
@@ -8,6 +8,53 @@
 // Internal
 #include "Utils/Buffer2D.hpp"
 #include "Window/Window.hpp"
+#include "Camera/Camera.hpp"
+#include "Geometry/Mesh.hpp"
+#include "Camera/Camera.hpp"
+#include "Shader/Shader.hpp"
+
+
+// Shader uniforms
+typedef struct {
+  glm::mat4 m;
+  glm::mat4 mvp;
+} Uniforms_t;
+
+
+// Raster parameters
+typedef struct {
+  glm::vec3 nml_ws;    // Vertex normal in world space
+} RParams_t;
+
+
+// Vertex shader, returns position of vertex in homogenous coordinates
+glm::vec4 VS(Uniforms_t& uniforms, retch::Vertex& vertex, RParams_t& vsOut) {
+
+  // Pass normal directly for now
+  vsOut.nml_ws = vertex.normal;
+
+  // Return position of translated vertex
+  return uniforms.mvp * glm::vec4(vertex.position, 1.0f);
+}
+
+
+// Fragment shader returns RGBA output
+glm::vec4 FS(
+  Uniforms_t& uniforms,
+  RParams_t& fsIn) {
+
+  return glm::vec4(fsIn.nml_ws, 1);
+}
+
+
+retch::Mesh GenTestMesh(void) {
+  std::vector<retch::Vertex> vertices = {
+    {glm::vec3(0, 0.5, 0),      glm::vec2(0, 0),  glm::vec3(1, 0, 0)},
+    {glm::vec3(-0.5, -0.5, 0),  glm::vec2(0, 0),  glm::vec3(0, 1, 0)},
+    {glm::vec3(0.5, -0.5, 0),   glm::vec2(0, 0),  glm::vec3(0, 0, 1)}};
+  retch::Mesh mesh(vertices);
+  return mesh;
+}
 
 
 // Add command line optioons to the option parser
@@ -33,24 +80,35 @@ int main(int argc, char **argv) {
   int displayy = opt.Get("displayy");
   retch::Window window(displayx, displayy, "retch");
 
-  // Create raw pixel buffers for the renderer
+  // Create raw pixel buffers for rendering
   float drawRatio = opt.Get("drawratio");
   int renderx = (float)displayx * drawRatio;
   int rendery = (float)displayy * drawRatio;
   retch::Buffer2D<glm::vec4> frameBuffer(renderx, rendery);
   retch::Buffer2D<float> depthBuffer(renderx, rendery);
 
-  // Set some random pixel colours (TEMPORARY, for testing)
-  for(int i = 0; i < renderx; i++) {
-    for(int j = 0; j < rendery; j++) {
-      frameBuffer[j][i].x = (float)(rand() % 255) / 255;
-      frameBuffer[j][i].y = (float)(rand() % 255) / 255;
-      frameBuffer[j][i].z = (float)(rand() % 255) / 255;
-    }
-  }
+  // Set up shader and camera
+  retch::Shader<Uniforms_t, RParams_t> shader(VS, FS);
+  Uniforms_t uniforms;
+  uniforms.m = glm::mat4(1.0f);
+
+  // Set up the camera
+  retch::Camera camera;
+  camera.SetViewRatio((float)renderx / (float)rendery);
+  camera.SetPosition({0, 0, -2});
+
+  // Generate a test mesh
+  retch::Mesh mesh = GenTestMesh();
 
   // Draw loop
   while(!window.Done()) {
+    frameBuffer.Clear(glm::vec4(0.0f));
+    depthBuffer.Clear(1.0f);
+
+    uniforms.m = glm::rotate(uniforms.m, (float)0.1, glm::vec3(0, 1, 0));
+    uniforms.mvp = camera.projMat * camera.viewMat * uniforms.m;
+    shader.Draw(frameBuffer, depthBuffer, uniforms, mesh);
+
     window.Output(frameBuffer);
     window.Refresh();
   }
