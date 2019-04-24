a6bd8b1870a533abca77382acc4aa9f5a11883f1
ac101m <mofty101@gmail.com>
2019-04-24 03:08:18 +0100

    Implement simple diffuse shader

--- src/retch.cpp
+++ src/retch.cpp
@@ -16,37 +16,49 @@
 
 // Shader uniforms
 typedef struct {
-  glm::mat4 m;
-  glm::mat4 mvp;
+  glm::mat4 mMx;
+  glm::mat4 mvpMx;
+  glm::vec3 aLightPow;      // Ambient light power
+  glm::vec3 pLightPow;      // Point light power
+  glm::vec3 pLightPos_ws;   // Point light position in worldspace
+  glm::vec3 objectColour;
 } Uniforms_t;
 
 
 // Raster parameters
 typedef struct {
-  glm::vec3 nml_ws;    // Vertex normal in world space
+  glm::vec3 pos_ws;         // Position in world space
+  glm::vec3 nml_ws;         // Normal in world space
 } RParams_t;
 
 
 // Vertex shader, returns position of vertex in homogenous coordinates
 glm::vec4 VS(Uniforms_t& uniforms, retch::Vertex& vertex, RParams_t& vsOut) {
-
-  // Pass normal directly for now
-  vsOut.nml_ws = vertex.normal;
-
-  // Return position of translated vertex
-  return uniforms.mvp * glm::vec4(vertex.position, 1.0f);
+  vsOut.pos_ws = uniforms.mMx * glm::vec4(vertex.position, 1.0f);
+  vsOut.nml_ws = uniforms.mMx * glm::vec4(vertex.normal, 1.0f);
+  return uniforms.mvpMx * glm::vec4(vertex.position, 1.0f);
 }
 
 
 // Fragment shader returns RGBA output
-glm::vec4 FS(
-  Uniforms_t& uniforms,
-  RParams_t& fsIn) {
+glm::vec4 FS(Uniforms_t& uniforms,RParams_t& fsIn) {
 
-  return glm::vec4(fsIn.nml_ws, 1);
+  // Ambient component
+  glm::vec3 lAmbient = uniforms.aLightPow;
+
+  // Diffuse component
+  glm::vec3 dir = uniforms.pLightPos_ws - fsIn.pos_ws;
+  float d2 = (dir.x * dir.x) + (dir.y * dir.y) + (dir.z * dir.z);
+  float angle = std::max(glm::dot(fsIn.nml_ws, glm::normalize(dir)), 0.0f);
+  glm::vec3 lDiffuse = angle * (uniforms.pLightPow / d2);
+
+  // Add up lighting components
+  glm::vec3 lTotal = (lAmbient + lDiffuse) * uniforms.objectColour;
+  return glm::vec4(lTotal, 1.0f);
 }
 
 
+// Simple single triangle test mesh
 retch::Mesh GenTestMesh(void) {
   std::vector<retch::Vertex> vertices = {
     {glm::vec3(0, 0.5, 0),      glm::vec2(0, 0),  glm::vec3(1, 0, 0)},
@@ -90,24 +102,30 @@ int main(int argc, char **argv) {
   // Set up shader and camera
   retch::Shader<Uniforms_t, RParams_t> shader(VS, FS);
   Uniforms_t uniforms;
-  uniforms.m = glm::mat4(1.0f);
+  uniforms.mMx = glm::mat4(1);
+  uniforms.aLightPow = glm::vec3(0.1);
+  uniforms.pLightPow = glm::vec3(50);
+  uniforms.pLightPos_ws = glm::vec3(7, 7, 4);
+  uniforms.objectColour = glm::vec3(1, 1, 1);
 
   // Set up the camera
   retch::Camera camera;
   camera.SetViewRatio((float)renderx / (float)rendery);
-  camera.SetPosition({0, 0, -2});
+  camera.SetPosition(glm::vec3(0, 1.5, -6));
 
-  // Generate a test mesh
-  retch::Mesh mesh = GenTestMesh();
+  // Generate test meshes
+  retch::Mesh triangle = GenTestMesh();
+  retch::Mesh teapot("data/Objects/Teapot.obj");
+  teapot.ComputeNormals();
 
   // Draw loop
   while(!window.Done()) {
     frameBuffer.Clear(glm::vec4(0.0f));
     depthBuffer.Clear(1.0f);
 
-    uniforms.m = glm::rotate(uniforms.m, (float)0.1, glm::vec3(0, 1, 0));
-    uniforms.mvp = camera.projMat * camera.viewMat * uniforms.m;
-    shader.Draw(frameBuffer, depthBuffer, uniforms, mesh);
+    uniforms.mMx = glm::rotate(uniforms.mMx, (float)0.002, glm::vec3(0, 1, 0));
+    uniforms.mvpMx = camera.projMat * camera.viewMat * uniforms.mMx;
+    shader.Draw(frameBuffer, depthBuffer, uniforms, teapot);
 
     window.Output(frameBuffer);
     window.Refresh();
